95b52fffd2926f6090f2f01f998c650a7d6510c4
hatzybt <trung.nt239620@sis.hust.edu.vn>
2026-05-08 10:15:15 +0700

    update db.sql with friend table

--- db.sql
+++ db.sql
@@ -30,6 +30,23 @@ CREATE TABLE `account` (
   `description` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `friend`
+--
+
+DROP TABLE IF EXISTS `friend`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `friend` (
+  `account_id` int NOT NULL,
+  `friend_id` int NOT NULL,
+  PRIMARY KEY (`account_id`,`friend_id`),
+  KEY `friend_id` (`friend_id`),
+  CONSTRAINT `friend_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `friend_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -42,4 +59,4 @@ CREATE TABLE `account` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-07 10:28:37
+-- Dump completed on 2026-05-08  9:07:32
